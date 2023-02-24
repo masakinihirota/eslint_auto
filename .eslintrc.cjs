0f5d792329510cad613b80a44f743b09744f801a
masakinihirota <masakinihirota@gmail.com>
2023-02-24 19:23:08 +0900

    動作確認済み

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
   parserOptions: {
     project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "import", "unused-imports"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
@@ -23,6 +23,28 @@ module.exports = {
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
+      },
+    ],
+    "unused-imports/no-unused-imports": "warn",
+    "import/order": [
+      // 順番が間違っていた時の警告方法
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        // それぞれのgroupsとの間は1行分空ける。
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        // 大文字小文字関係なくアルファベット順にする。
+        alphabetize: { order: "asc", caseInsensitive: true },
       },
     ],
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import { type NextPage } from "next";
-import { Head } from "next/document";
 import Link from "next/link";
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
